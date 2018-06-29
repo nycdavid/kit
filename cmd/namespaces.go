fa5aaec32fa7fbcc926b055ff842c96965b80fec
David Ko <david.ko@velvetreactor.com>
2018-06-29 16:37:12 -0400

    add set credentials command

--- cmd/namespaces.go
+++ cmd/namespaces.go
@@ -15,6 +15,12 @@ var namespacesCmd = &cobra.Command{
 	Use:   "namespaces",
 	Short: "List all namespaces in your Kubernetes cluster",
 	Run: func(cmd *cobra.Command, args []string) {
+		cmdS := exec.Command([]string{
+			"config",
+			"set-credentials",
+			""
+		})
+			// "kubectl config set-credentials ${KUBE_CLUSTER}-basicauth --username=${KUBE_USER} --password=${KUBE_PASSWORD}")
 		cmdN := exec.Command("kubectl", "get", "namespaces")
 		com.Stdout = os.Stdout
 		com.Stderr = os.Stderr
